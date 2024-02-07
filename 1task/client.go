cb193a35aad346323fa65940ca9839dca55ae7df
Ppolyak <pasha_polyakov8@mail.ru>
2024-02-08 00:19:55 +0300

    gorm to sql

--- 1task/client.go
+++ 1task/client.go
@@ -1,105 +1,29 @@
 package main
 
 import (
+	_ "encoding/json"
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	_ "github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "io"
 	"log"
 	"net/http"
 	"time"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = ""
-	dbname   = "postgres"
-)
-
-type User struct {
-	gorm.Model
-	ID       int
-	Username string         `json:"username"`
-	Password string         `json:"password"`
-	Token    string         `gorm:"not null"`
-	Session  SessionStorage `gorm:"foreignKey:UserID" json:"-"`
-	Posts    []Post         `gorm:"foreignKey:UserID"` // Отношение к постам пользователя
-}
-
-type Post struct {
-	gorm.Model
-	ID      int
-	Title   string `json:"title" gorm:"not null"`
-	Content string `json:"content" gorm:"not null"`
-	UserID  int    `json:"-"`
-	User    User   `gorm:"foreignKey:UserID" json:"-"`
-}
-
-type Comment struct {
-	gorm.Model
-	PostID uint
-	UserID uint
-	/*Text   string
-	User   User*/
-}
-
-type SessionStorage struct {
-	gorm.Model
-	UserID int    `gorm:"uniqueIndex;not null"`
-	Token  string `gorm:"not null"`
-}
-
-type Tag struct {
-	gorm.Model
-	Name string
-	/*Post []Post*/
-}
-
-/*func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if isValidToken(token) {
-			next.ServeHTTP(w, r)
-		} else {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		}
-	})
-}*/
-
-/*func isValidToken(token string) bool {
-	return token != ""
-}*/
-
-var db *gorm.DB
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	var err error
-	db, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	/*db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	*/
-	db.Debug().Migrator().DropTable(&User{}, &SessionStorage{}, &Comment{}, &Post{}, &SessionStorage{})
-
-	db.AutoMigrate(&Post{}, &User{}, &Comment{}, &Tag{}, &SessionStorage{})
+	newPostgres()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/users/register", registerUser).Methods("POST")
 	r.HandleFunc("/users/login", loginUser).Methods("POST")
 	r.HandleFunc("/posts", createPost).Methods("POST")
-	r.HandleFunc("/posts", getAllPosts).Methods("GET")
-	r.HandleFunc("/posts/{id}", getPostById).Methods("GET")
-	r.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	//r.HandleFunc("/posts", getAllPosts).Methods("GET")
+	//r.HandleFunc("/posts/{id}", getPostById).Methods("GET")
+	//r.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 
 	port := ":6262"
 	fmt.Printf("Сервер запущен на порту %s...\n", port)
@@ -107,35 +31,51 @@ func main() {
 	http.ListenAndServe(port, r)
 }
 
-func generateToken(user User, secretKey string) (string, error) {
-
+func generateToken(user User) (string, error) {
 	claims := jwt.MapClaims{
-		"sub": user,
-		"exp": time.Now().Add(time.Hour * 24).Unix(), // Время жизни токена
-		"iat": time.Now().Unix(),                     // Время создания токена
+		"sub": user.ID, // Предполагается, что ID пользователя является числом
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"iat": time.Now().Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	tokenString, err := token.SignedString([]byte(secretKey))
+	tokenString, err := token.SignedString([]byte("your_secret_key"))
 	if err != nil {
-		return "err", nil
+		return "", err
 	}
 
-	return tokenString, err
+	return tokenString, nil
 }
 
 func authenticateUser(r *http.Request) (*User, error) {
 	token := r.Header.Get("Authorization")
-	var session SessionStorage
 
-	if err := db.Where("token = ?", token).First(&session).Error; err != nil {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("your_secret_key"), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sub, ok := claims["sub"].(float64)
+	if !ok {
+		return nil, errors.New("invalid token")
+	}
+
+	userID := int(sub)
+
+	var session SessionStorage
+	log.Println(userID)
+	err = db.Get(&session, "SELECT * FROM session_storage WHERE user_id = $1", userID)
+	if err != nil {
 		return nil, err
 	}
 
 	var user User
-
-	if err := db.Where("id = ?", session.UserID).First(&user).Error; err != nil {
+	err = db.Get(&user, "SELECT * FROM users WHERE id = $1", session.UserID)
+	if err != nil {
 		return nil, err
 	}
 
--- 1task/hand.go
+++ 1task/hand.go
@@ -2,62 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"github.com/gorilla/mux"
-	"gorm.io/gorm"
+	_ "github.com/jmoiron/sqlx"
 	"io"
+	_ "io/ioutil"
+	"log"
 	"net/http"
 )
 
 func registerUser(w http.ResponseWriter, r *http.Request) {
 	var user User
-
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Error reading request body", http.StatusInternalServerError)
 		return
 	}
-
-	if err := json.Unmarshal(body, &user); err != nil {
-		http.Error(w, "Error while decoding JSON", http.StatusBadRequest)
-		return
-	}
-
-	if user.Username == "" {
-		http.Error(w, "Missing username in JSON", http.StatusBadRequest)
-		return
-	}
-
-	if user.Password == "" {
-		http.Error(w, "Missing username in JSON", http.StatusBadRequest)
-		return
-	}
-
-	if len(user.Username) < 5 {
-		http.Error(w, "Username must be at least 5 characters long", http.StatusBadRequest)
-		return
-	}
-
-	db.Create(&user)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(user); err != nil {
-		http.Error(w, "Error while encoding JSON", http.StatusInternalServerError)
-	}
-
-	/*generateToken(user, "secret")*/
-}
-
-func loginUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
 	defer r.Body.Close()
 
 	if err := json.Unmarshal(body, &user); err != nil {
@@ -65,43 +23,61 @@ func loginUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if trimmedUsername := user.Username; trimmedUsername == "" || user.Password == "" {
-		http.Error(w, "Username and Password cannot be empty", http.StatusBadRequest)
+	if user.Username == "" || user.Password == "" {
+		http.Error(w, "Username and password cannot be empty", http.StatusBadRequest)
 		return
 	}
 
-	var logUser User
-	if err := db.Where("username = ?", user.Username).First(&logUser).Error; err != nil {
+	db.MustExec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, user.Password)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if err := json.Unmarshal(body, &user); err != nil {
+		http.Error(w, "Error while decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	if user.Username == "" || user.Password == "" {
+		http.Error(w, "Username and password cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	err = db.Get(&user, "SELECT id, username FROM users WHERE username = $1 AND password = $2", user.Username, user.Password)
+	if err != nil {
 		http.Error(w, "Invalid login or password", http.StatusUnauthorized)
 		return
 	}
 
-	if logUser.Password != user.Password {
-		http.Error(w, "Invalid login or password", http.StatusUnauthorized)
-		return
-	}
-
-	token, err := generateToken(logUser, "your_secret_key")
+	token, err := generateToken(user)
 	if err != nil {
 		http.Error(w, "Error generating token", http.StatusInternalServerError)
 		return
 	}
 
-	logUser.Token = token
+	user.Token = token
 
-	session := SessionStorage{
-		UserID: logUser.ID,
-		Token:  token,
+	_, err = db.Exec("INSERT INTO session_storage (user_id, token) VALUES ($1, $2)", user.ID, user.Token)
+	if err != nil {
+		log.Println("Error updating session storage:", err)
+		http.Error(w, "Error updating session storage", http.StatusInternalServerError)
+		return
 	}
-	db.Create(&session)
-
-	db.Save(&logUser)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(logUser); err != nil {
-		http.Error(w, "Error while encoding JSON", http.StatusInternalServerError)
-	}
+	json.NewEncoder(w).Encode(user)
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
@@ -111,7 +87,6 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error reading request body", http.StatusInternalServerError)
 		return
 	}
-
 	defer r.Body.Close()
 
 	if err := json.Unmarshal(body, &post); err != nil {
@@ -121,9 +96,12 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 
 	user, err := authenticateUser(r)
 	if err != nil {
+		log.Println("Authentication failed:", err) // Логирование ошибки аутентификации
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
 		return
 	}
+
+	log.Println("Authenticated user:", user.ID) // Логирование идентификатора аутентифицированного пользователя
 
 	post.UserID = user.ID
 
@@ -132,16 +110,22 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	db.Create(&post)
+	_, err = db.Exec("INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3)", post.Title, post.Content, post.UserID)
+	if err != nil {
+		log.Println("Error creating post:", err) // Логирование ошибки создания поста
+		http.Error(w, "Error creating post", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(post); err != nil {
+		log.Println("Error encoding JSON:", err) // Логирование ошибки кодирования JSON
 		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
 	}
-
 }
 
-func getAllPosts(w http.ResponseWriter, r *http.Request) {
+/*func getAllPosts(w http.ResponseWriter, r *http.Request) {
 	user, err := authenticateUser(r)
 	if err != nil {
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
@@ -247,4 +231,4 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
 	}
 
-}
+}*/
